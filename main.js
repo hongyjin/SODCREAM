28b1887a2f1c697dbd6470c4aa2f2701ab1b2e27
wodus <wodustjdus34@gmail.com>
2023-07-21 20:55:58 +0900

    Error clear ejs파일에 css파일 적용

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const express = require("express");//express 요청
 const app = express();
 const path = require('path');
-const homeController = require("../controllers/homeController");
+const homeController = require("./controllers/homeController");
 const layouts = require("express-ejs-layouts");
 
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs"); //ejs 사용
 
 app.use(express.static("public")); //외부에서 public 디렉토리 접근 가능
@@ -21,8 +21,6 @@ app.use(express.json()); // Express 미들웨어 설정
 //URL 연결
 console.log(homeController);
 //app.get("/hobby", homeController.showHobby); //취미
-//app.get("/login", homeController.showLogin); //로그
-//app.get("/hobbyAuction", homeController.showhobbyAuction); //경매
 //app.get("/error", errorController.pageNotFoundError); //에러
 app.get("/", (req, res) => { //루트 라우트 생성
         res.render("Home");
