6a4d9f3e211f96304dbbf10ce387f879c8de44d6
wodus <wodustjdus34@gmail.com>
2023-07-21 19:22:06 +0900

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const express = require("express");//express 요청
+const app = express();
+const homeController = require("../controllers/homeController");
+const layouts = require("express-ejs-layouts");
+
+app.set("view engine", "ejs"); //ejs 사용
+app.set("views", path.join(__dirname, "views"));
+app.use(layouts); //레이아웃 등록
+
+//app.use(express.static("public")); //외부에서 public 디렉토리 접근 가능
+app.set("port", process.env.PORT || 3000);
+
+//body-parser 추가
+app.use(
+        express.urlencoded({
+                extended: false
+        })
+);
+app.use(express.json()); // Express 미들웨어 설정
+
+//URL 연결
+console.log(homeController);
+//app.get("/hobby", homeController.showHobby); //취미
+//app.get("/login", homeController.showLogin); //로그
+//app.get("/hobbyAuction", homeController.showhobbyAuction); //경매
+//app.get("/error", errorController.pageNotFoundError); //에러
+app.get("/", (req, res) => { //루트 라우트 생성
+        res.render("gerneralHome");
+});
+
+//app.use(errorController.pageNotFoundError); //에러 처리 추
+//app.use(errorController.internalServerError);
+
+app.listen(app.get("port"), () => { //80번 포트 리스닝 설정
+        console.log(`Server running at http://localhost:${app.get("port")}`);
+});
