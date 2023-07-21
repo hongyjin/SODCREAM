26a7ac255af9e9d8802f43a92ce81a0fd596f242
wodus <wodustjdus34@gmail.com>
2023-07-21 19:56:22 +0900

    Create Home.ejs

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const layouts = require("express-ejs-layouts");
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs"); //ejs 사용
 
-//app.use(express.static("public")); //외부에서 public 디렉토리 접근 가능
+app.use(express.static("public")); //외부에서 public 디렉토리 접근 가능
 app.set("port", process.env.PORT || 3000);
 
 //body-parser 추가
@@ -25,7 +25,7 @@ console.log(homeController);
 //app.get("/hobbyAuction", homeController.showhobbyAuction); //경매
 //app.get("/error", errorController.pageNotFoundError); //에러
 app.get("/", (req, res) => { //루트 라우트 생성
-        res.render("gerneralHome");
+        res.render("Home");
 });
 
 //app.use(errorController.pageNotFoundError); //에러 처리 추
