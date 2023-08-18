689ca16e32281835e29346b840d1fb44c544a1d7
hongy <simplehuj@naver.com>
2023-08-18 14:27:18 +0900

    Do dic.popup

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("popupButton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  document.getElementById("closeButton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+  });
+  
