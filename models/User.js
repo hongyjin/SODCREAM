2682428a637fe36d6c787460e0018f00055c4540
wodus <wodustjdus34@gmail.com>
2023-08-18 14:27:47 +0900

    Upadate 로그인 기능 구현

--- models/User.js
+++ models/User.js
@@ -1,41 +1,41 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('User', {
-    userId: {
-      type: DataTypes.STRING(8),
-      allowNull: false,
-      primaryKey: true
-    },
-    password: {
-      type: DataTypes.STRING(20),
-      allowNull: true
-    },
-    userName: {
-      type: DataTypes.STRING(20),
-      allowNull: true
-    },
-    point: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    },
-    mainCharName: {
-      type: DataTypes.STRING(10),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'User',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "userId" },
-        ]
-      },
+    return sequelize.define('User', {
+        userId: {
+            type: DataTypes.STRING(8),
+            allowNull: false,
+            primaryKey: true
+          },
+          password: {
+            type: DataTypes.STRING(20),
+            allowNull: true
+          },
+          userName: {
+            type: DataTypes.STRING(20),
+            allowNull: true
+          },
+          point: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+          },
+          mainCharName: {
+            type: DataTypes.STRING(10),
+            allowNull: true
+          }
+        }, {
+        sequelize,
+        tableName: 'User',
+        timestamps: false,
+        indexes: [
+        {
+            name: "PRIMARY",
+            unique: true,
+            using: "BTREE",
+            fields: [
+                { name: "userId" },
+            ]
+        },
     ]
   });
 };
